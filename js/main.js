23383a44ed91c2008e271612ba861cfee3b2586c
unknown <leitnergaston@gmail.com>
2023-03-30 23:56:11 -0300

    actualizacion 30/3/23 skills

--- js/main.js
+++ js/main.js
@@ -10,7 +10,26 @@
 
 
 /*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target'),
+    tabContent = document.querySelectorAll('[data-content')
 
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            const target = document.querySelector(tab.dataset.target)
+
+            tabContent.forEach(tabContents => {
+                tabContents.classList.remove('skills-active')
+            })
+
+            target.classList.add('skills-active')
+
+            tabs.forEach(tab => {
+                tab.classList.remove('skills-active')
+            })
+
+            tab.classList.add('skills-active')
+        })
+    })
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
