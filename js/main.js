23c9c136c06fd3cea29d42078b1bb5451972babd
unknown <leitnergaston@gmail.com>
2023-07-04 18:35:37 -0300

    act 4/7/23

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 /*=============== PRELOADER ===============*/
-window.onload = function(){
-    alert('hola');
-    $('#onload').fadeOut();
-    $('body').removeClass('body-hidden');
+window.addEventListener('load', () => {
+    const contenedor_loader = document.querySelector('.containerLoader')
+    contenedor_loader.style.opacity = 0
+    contenedor_loader.style.visibility = 'hidden'
+})
 
-}
+
 
 
 
