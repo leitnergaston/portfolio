da09f9452324d2b1eae1177b444502192e62240a
Gaston <leitnergaston@gmail.com>
2024-02-07 12:38:55 -0300

    cambio codigo de formulario de contacto

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,22 +1,34 @@
-    const $form = document.querySelector('#form')
+    // const $form = document.querySelector('#form')
 
-    $form.addEventListener('submit', handleSubmit)
+    // $form.addEventListener('submit', handleSubmit)
 
-    async function handleSubmit(event) {
-        event.preventDefault()
-        const form = new FormData(this)
-        const response = await fetch(this.action, {
-            method: this.method,
-            body: form,
-            headers: {
-                'Accept': 'application/json'
-            }
-        })
-        if (response.ok) {
-            this.reset()
-            msg.innerHTML = "✔ Mensaje Enviado"
-            setTimeout(function(){
-                msg.innerHTML = ""
-            },4000)
-        }
-    }
+    // async function handleSubmit(event) {
+    //     event.preventDefault()
+    //     const form = new FormData(this)
+    //     const response = await fetch(this.action, {
+    //         method: this.method,
+    //         body: form,
+    //         headers: {
+    //             'Accept': 'application/json'
+    //         }
+    //     })
+    //     if (response.ok) {
+    //         this.reset()
+    //         msg.innerHTML = "✔ Mensaje Enviado"
+    //         setTimeout(function(){
+    //             msg.innerHTML = ""
+    //         },4000)
+    //     }
+    // }
+
+    import { Resend } from 'resend';
+
+    const resend = new Resend('API_KEY');
+
+    resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: 'leitnergaston@gmail.com',
+        subject: 'Nuevo mail de portafolio',
+        html: '<p>✔ Mensaje enviado</p>'
+    });
+    
